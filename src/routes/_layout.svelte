f3e2fdb7f6d832f7e3bca92636e44632ecb3e3a9
Helmar Bachle <42181669+helmar81@users.noreply.github.com>
2023-03-18 06:41:54 +0100

    first commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Nav from '../components/Nav.svelte';
+	import "../app.css";
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: rgb(235, 229, 227);
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/about.svelte
@@ -0,0 +1,25 @@
+<script>
+import meLaptop from 'images/meLaptop.jpg';	
+import successkid from 'images/successkid.jpg';
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+	<figure>
+		<figcaption class="text-sky-400/100 text-center ">About me</figcaption>
+		<img class="inline-block h-62 w-52 rounded-full ring-2  ring-white" alt="Helmar Baechle" src="{meLaptop}">
+		
+	</figure>
+	
+
+<p class="text-justify text-sky-400/100 py-6 font-medium mb-1">As a Level 3 Support associate for Amazon, I excel at handling complex queries and escalated issues from our premium sellers. 
+	In my spare time, I am passionate about staying up-to-date on the latest technologies and enjoy building apps with Astro.js. 
+	With a love for travel, I have visited 65 countries and bring a diverse perspective to my work. Whether at home or abroad, 
+	I am always seeking new challenges and opportunities to grow.</p>
+
+	<div class="mt-3 text-sm font-medium">
+		<a href="https://mytopcountries.web.app/" target="blank"class="text-blue-500">Travel Blog</a>
+	  </div>
+	
